06b1c604c9472f63d4c32979c53f17d91de7a451
inachen <ina.chen@ucsf.edu>
2014-10-20 15:31:39 -0700

    testing biopython

---
+++ testbio.py
@@ -0,0 +1,18 @@
+import sys
+# import matplotlib
+# matplotlib.use('Agg')
+# import matplotlib.pyplot as plt
+# import collections
+# import csv
+# import numpy as np
+# import math
+# import matplotlib.cm as cm
+# from mpltools import style
+# from mpltools import color
+import cPickle as pic
+
+
+from Bio import SeqIO
+
+for record in SeqIO.parse(open("test.fastq", "rU"), "fastq"):
+	print record.description
