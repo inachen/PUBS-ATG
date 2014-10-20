de0f23096a919c4ca47a843e756c83f702ffa734
brukmensa <brukmensa@gmail.com>
2014-10-20 15:31:20 -0700

    testfile generation

---
+++ trial.py
@@ -0,0 +1,13 @@
+import sys
+
+i=0
+f = open("/data/ClassData-2014/data-oct16/lane1_Undetermined_L001_R1_001.fastq", "r")
+copy = open("/data/atg/testfile.fastq", "wt")
+
+for i in range(1,1001):
+	line = f.readline()
+	copy.write(str(line))
+f.close()
+copy.close()
+
+
