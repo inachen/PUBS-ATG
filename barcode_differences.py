f318c319b48f6ba95c4095a4f91c4d5436828336
stevenmo5241 <steven.moss524@gmail.com>
2014-10-22 16:54:24 -0700

    update

--- barcode_differences.py
+++ barcode_differences.py
@@ -1,10 +1,15 @@
 barcodes = ['CGTGAT', 'ACATCG', 'GCCTAA','TGGTCA','CACTGT','ATTGGC','GATCTG','TCAAGT','CTGATC','AAGCTA','GTAGCC','TACAAG','TTGACT','GGAACT','TGACAT','GGACGG','CTCTAC','GCGGAC','TTTCAC','GGCCAC','CGAAAC','CGTACG','CCACTC','GCTACC','ATCAGT','GCTCAT','AGGAAT','CTTTTG','TAGTTG','CCGGTG']
 import numpy as np
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import cPickle as pic
 data = pic.load(open("allele_dic_with_WT.pkl", "rb"))
 alleles = []
 alleles = data.keys()
+allele_set = []
+for i in range (0, 1001):
+	allele_set.append(alleles[i])
 
 #comparing one string to another and returning the number of differences
 def single_comp(cats, dogs):
@@ -37,6 +42,17 @@ def mult_comp_list(seq1, seq2):
 			n = single_comp(seq1[i], seq2[j])
 			num_list.append(n)
 	return num_list
+
+#takes two strings and outputs a list of any two sequences that are 1 or 2 hamming distance apart
+def list_small_dist (seq1, seq2):
+	single_diff = []
+	for i in range(0, len(seq1)):
+		for j in range(0, len(seq2)):
+			n = 0
+			n = single_comp(seq1[i], seq2[j])
+			if n == 1 or n == 2:
+				single_diff.append(seq1[i] + "-" + seq2[j])
+	return single_diff
 	
 #comparing two lists of strings and outputting the smallest non-zero hamming distance
 def mult_comp_small(seq1, seq2):
@@ -62,10 +78,17 @@ def check(seq1, seq2):
 	
 #print single_comp("cats", "dogs")
 
-#num_list = mult_comp_list(alleles, alleles)
-#plt.hist(num_list)
+num_list = mult_comp_list(alleles, alleles)
+plt.hist(num_list)
+plt.title('Hamming Distances of Barcodes')
+plt.xlabel('Hamming Distance Values')
+plt.ylabel('Number of Occurances')
 #plt.show()
-
+plt.savefig('barcodes_histogram')
+#small_dist = list_small_dist(allele_set, allele_set)
+#print small_dist
+#pic.dump(small_dist, open('test_dump.pkl', 'wb'))
+#print len(alleles)
 #print mult_comp_small(alleles, alleles)
 
 #print check(barcodes, barcodes)
---
+++ printpickles.py
@@ -0,0 +1,11 @@
+import cPickle as pic
+#data = pic.load(open("allele_dic (1).pkl", "rb"))
+#print data
+
+import cPickle as pic
+#data1 = pic.load(open("aminotonumber.pkl", "rb"))
+#print data1
+
+import cPickle as pic
+data2 = pic.load(open("test_dump.pkl", "rb"))
+print data2
