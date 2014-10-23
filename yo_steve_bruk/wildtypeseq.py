bdf14fffa9b268df3fae0e70fd1b3fbb5db3d3a5
stevenmo5241 <steven.moss524@gmail.com>
2014-10-23 13:43:49 -0700

    enrichment values

--- yo_steve_bruk/wildtypeseq.py
+++ yo_steve_bruk/wildtypeseq.py
@@ -2,7 +2,7 @@ import cPickle as pic
 
 ubiquitin = "MQIFVKTLTGKTITLEVESSDTIDNVKSKIQDKEGIPPDQQRLIFAGKQLEDGRTLSDYNIQKESTLHLVLRLRGG"
 testset = pic.load(open("2500_filtered_seq.pkl", "rb"))
-wildtypedict = dict(((i, ubiquitin[i]),([],[])) for i in range (1,76))
+wildtypedict = dict(((i, ubiquitin[i-1]),([],[])) for i in range (1,76))
 wildtypedict["(76, 'STOP')"]=([],[])
 
 count = 0
