9095d34ecd1a62bc60b2939f7dd3aecbf1a889b1
inachen <ina.w.chen@gmail.com>
2014-11-12 13:26:59 -0800

    push

---
+++ rosetta/mapping_correlation2.r
@@ -0,0 +1,224 @@
+
+# find-replace all "Monomer", "CUE", "OTU", "RPN13", "SH3", "UQCON"
+
+# Monomer
+# all residues
+all_dat = read.csv('out_pickles/all_Monomer_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+
+pdf(file='plots/all_Monomer.pdf')
+plot(log2(all_dat), pch=16)
+title("Monomer")
+dev.off()
+
+# hydrophobic
+charged_dat = read.csv('out_pickles/charged_Monomer_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+hydrophbic_dat = read.csv('out_pickles/hydrophobic_Monomer_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+polar_dat = read.csv('out_pickles/polar_Monomer_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+
+pdf(file='plots/hydrophobic_Monomer.pdf')
+
+plot(log2(charged_dat), pch=16, col='darkgoldenrod1')
+points(log2(hydrophbic_dat), col='lightblue4', pch=16)
+points(log2(polar_dat), col='indianred1', pch=16)
+title("Monomer")
+
+legend( 12, 3,c("Charged","Hydrophobic", "Polar"), pch = 16, col=c("darkgoldenrod1", "lightblue4","indianred1"))
+dev.off()
+
+# buried
+buried_dat = read.csv('out_pickles/buried_Monomer_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+surface_dat = read.csv('out_pickles/surface_Monomer_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+
+pdf(file='plots/buried_Monomer.pdf')
+
+plot(log2(buried_dat), col='lightblue4', pch=16)
+points(log2(surface_dat), col='indianred1', pch=16)
+title("Monomer")
+
+legend( 16, 3,c("Buried", "Surface"), pch = 16, col=c("lightblue4","indianred1"))
+dev.off()
+
+# CUE
+# all residues
+all_dat = read.csv('out_pickles/all_CUE_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+
+pdf(file='plots/all_CUE.pdf')
+plot(log2(all_dat), pch=16)
+title("CUE")
+dev.off()
+
+# hydrophobic
+charged_dat = read.csv('out_pickles/charged_CUE_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+hydrophbic_dat = read.csv('out_pickles/hydrophobic_CUE_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+polar_dat = read.csv('out_pickles/polar_CUE_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+
+pdf(file='plots/hydrophobic_CUE.pdf')
+
+plot(log2(charged_dat), pch=16, col='darkgoldenrod1')
+points(log2(hydrophbic_dat), col='lightblue4', pch=16)
+points(log2(polar_dat), col='indianred1', pch=16)
+title("CUE")
+
+legend( 12, 3,c("Charged","Hydrophobic", "Polar"), pch = 16, col=c("darkgoldenrod1", "lightblue4","indianred1"))
+dev.off()
+
+# buried
+buried_dat = read.csv('out_pickles/buried_CUE_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+surface_dat = read.csv('out_pickles/surface_CUE_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+
+pdf(file='plots/buried_CUE.pdf')
+
+plot(log2(buried_dat), col='lightblue4', pch=16)
+points(log2(surface_dat), col='indianred1', pch=16)
+title("CUE")
+
+legend( 16, 3,c("Buried", "Surface"), pch = 16, col=c("lightblue4","indianred1"))
+dev.off()
+
+# OTU
+# all residues
+all_dat = read.csv('out_pickles/all_OTU_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+
+pdf(file='plots/all_OTU.pdf')
+plot(log2(all_dat), pch=16)
+title("OTU")
+dev.off()
+
+# hydrophobic
+charged_dat = read.csv('out_pickles/charged_OTU_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+hydrophbic_dat = read.csv('out_pickles/hydrophobic_OTU_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+polar_dat = read.csv('out_pickles/polar_OTU_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+
+pdf(file='plots/hydrophobic_OTU.pdf')
+
+plot(log2(charged_dat), pch=16, col='darkgoldenrod1')
+points(log2(hydrophbic_dat), col='lightblue4', pch=16)
+points(log2(polar_dat), col='indianred1', pch=16)
+title("OTU")
+
+legend( 40, 3,c("Charged","Hydrophobic", "Polar"), pch = 16, col=c("darkgoldenrod1", "lightblue4","indianred1"))
+dev.off()
+
+# buried
+buried_dat = read.csv('out_pickles/buried_OTU_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+surface_dat = read.csv('out_pickles/surface_OTU_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+
+pdf(file='plots/buried_OTU.pdf')
+
+plot(log2(buried_dat), col='lightblue4', pch=16)
+points(log2(surface_dat), col='indianred1', pch=16)
+title("OTU")
+
+legend( 16, 3,c("Buried", "Surface"), pch = 16, col=c("lightblue4","indianred1"))
+dev.off()
+
+# RPN13
+# all residues
+all_dat = read.csv('out_pickles/all_RPN13_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+
+pdf(file='plots/all_RPN13.pdf')
+plot(log2(all_dat), pch=16)
+title("RPN13")
+dev.off()
+
+# hydrophobic
+charged_dat = read.csv('out_pickles/charged_RPN13_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+hydrophbic_dat = read.csv('out_pickles/hydrophobic_RPN13_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+polar_dat = read.csv('out_pickles/polar_RPN13_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+
+pdf(file='plots/hydrophobic_RPN13.pdf')
+
+plot(log2(charged_dat), pch=16, col='darkgoldenrod1')
+points(log2(hydrophbic_dat), col='lightblue4', pch=16)
+points(log2(polar_dat), col='indianred1', pch=16)
+title("RPN13")
+
+legend( 22, 3,c("Charged","Hydrophobic", "Polar"), pch = 16, col=c("darkgoldenrod1", "lightblue4","indianred1"))
+dev.off()
+
+# buried
+buried_dat = read.csv('out_pickles/buried_RPN13_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+surface_dat = read.csv('out_pickles/surface_RPN13_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+
+pdf(file='plots/buried_RPN13.pdf')
+
+plot(log2(buried_dat), col='lightblue4', pch=16)
+points(log2(surface_dat), col='indianred1', pch=16)
+title("RPN13")
+
+legend( 16, 3,c("Buried", "Surface"), pch = 16, col=c("lightblue4","indianred1"))
+dev.off()
+
+# SH3
+# all residues
+all_dat = read.csv('out_pickles/all_SH3_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+
+pdf(file='plots/all_SH3.pdf')
+plot(log2(all_dat), pch=16)
+title("SH3")
+dev.off()
+
+# hydrophobic
+charged_dat = read.csv('out_pickles/charged_SH3_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+hydrophbic_dat = read.csv('out_pickles/hydrophobic_SH3_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+polar_dat = read.csv('out_pickles/polar_SH3_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+
+pdf(file='plots/hydrophobic_SH3.pdf')
+
+plot(log2(charged_dat), pch=16, col='darkgoldenrod1')
+points(log2(hydrophbic_dat), col='lightblue4', pch=16)
+points(log2(polar_dat), col='indianred1', pch=16)
+title("SH3")
+
+legend(15, 3,c("Charged","Hydrophobic", "Polar"), pch = 16, col=c("darkgoldenrod1", "lightblue4","indianred1"))
+dev.off()
+
+# buried
+buried_dat = read.csv('out_pickles/buried_SH3_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+surface_dat = read.csv('out_pickles/surface_SH3_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+
+pdf(file='plots/buried_SH3.pdf')
+
+plot(log2(buried_dat), col='lightblue4', pch=16)
+points(log2(surface_dat), col='indianred1', pch=16)
+title("SH3")
+
+legend( 16, 3,c("Buried", "Surface"), pch = 16, col=c("lightblue4","indianred1"))
+dev.off()
+
+# UQCON
+# all residues
+all_dat = read.csv('out_pickles/all_UQCON_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+
+pdf(file='plots/all_UQCON.pdf')
+plot(log2(all_dat), pch=16)
+title("UQCON")
+dev.off()
+
+# hydrophobic
+charged_dat = read.csv('out_pickles/charged_UQCON_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+hydrophbic_dat = read.csv('out_pickles/hydrophobic_UQCON_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+polar_dat = read.csv('out_pickles/polar_UQCON_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+
+pdf(file='plots/hydrophobic_UQCON.pdf')
+
+plot(log2(charged_dat), pch=16, col='darkgoldenrod1')
+points(log2(hydrophbic_dat), col='lightblue4', pch=16)
+points(log2(polar_dat), col='indianred1', pch=16)
+title("UQCON")
+
+legend( 70, 3,c("Charged","Hydrophobic", "Polar"), pch = 16, col=c("darkgoldenrod1", "lightblue4","indianred1"))
+dev.off()
+
+# buried
+buried_dat = read.csv('out_pickles/buried_UQCON_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+surface_dat = read.csv('out_pickles/surface_UQCON_DDG_Fitness.csv', header=TRUE, check.names=FALSE)
+
+pdf(file='plots/buried_UQCON.pdf')
+
+plot(log2(buried_dat), col='lightblue4', pch=16)
+points(log2(surface_dat), col='indianred1', pch=16)
+title("UQCON")
+
+legend( 16, 3,c("Buried", "Surface"), pch = 16, col=c("lightblue4","indianred1"))
+dev.off()
--- rosetta/rosetta_ddg.py
+++ rosetta/rosetta_ddg.py
@@ -24,7 +24,17 @@ IN_DIR = "input"
 OUT_DIR = "out_pickles"
 FSEP = "/"
 FP_MONO = "uby_1ubq.json"
+FP_CUE = "uby_cue.json"
+FP_OTU = "uby_otu.json"
+FP_RPN13 = "uby_rpn13.json"
+FP_SH3 = "uby_sh3.json"
+FP_UQCON = "uby_uqcon.json"
 FITNESS_PKL = "D2S3fitness_scores.pkl"
+
+BINDING_LST = [("Monomer", FP_MONO), ("CUE", FP_CUE), ("OTU", FP_OTU),
+    ("RPN13", FP_RPN13), ("SH3", FP_SH3), ("UQCON", FP_UQCON)]
+
+BURIED = [3, 5, 15, 17, 23, 26, 30, 41, 43, 45, 50, 56, 59, 61, 67, 69]
 
 # parsing mutations A X1Y, 1 is position, Y is mutation
 POS_POS = 3
@@ -75,12 +85,25 @@ def bin_by_hydrophobe(ddg_dic):
 
     return hydro_bin_dic
 
-def bin_dic_to_csv(dic, fit_dic):
+def bin_by_topo(ddg_dic):
+    topo_bin_dic = {'buried': {}, 'surface':{}}
+
+    for k, val in ddg_dic.iteritems():
+        pos = k[0]
+
+        if pos in BURIED:
+            topo_bin_dic['buried'][k] = val
+        else:
+            topo_bin_dic['surface'][k] = val
+
+    return topo_bin_dic
+
+def bin_dic_to_csv(dic, fit_dic, name):
     
     for label, d in dic.iteritems():
         ddg_lst, fit_lst = dic_to_lsts(d, fit_dic)
 
-        lsts_to_csv(ddg_lst, fit_lst, 'DDG', 'Fitness', label)
+        lsts_to_csv(ddg_lst, fit_lst, 'DDG', 'Fitness', label, name)
 
 def dic_to_lsts(dic, fit_dic):
 
@@ -95,11 +118,11 @@ def dic_to_lsts(dic, fit_dic):
 
     return (ddg_lst, fit_lst)
 
-def lsts_to_csv(lst1, lst2, h1, h2, bintype):
+def lsts_to_csv(lst1, lst2, h1, h2, bintype, name):
 
     lst = [list(i) for i in zip(lst1, lst2)]
 
-    outfile = open(OUT_DIR + FSEP + bintype+"_"+h1+"_"+h2+".csv",'wb')
+    outfile = open(OUT_DIR + FSEP + bintype+"_"+name+"_"+h1+"_"+h2+".csv",'wb')
 
     wr = csv.writer(outfile)
     wr.writerow([h1, h2])
@@ -110,27 +133,33 @@ def run():
     # parameter value to extract
     param = "global_DDG"
 
-    # get ddg dic
-    ddg_dic = json_to_dic(IN_DIR+FSEP+FP_MONO, param)
-
-    pickle.dump(ddg_dic, open(OUT_DIR+FSEP+'ddg_dic.pkl', 'wb'))
-
-    # bin by hydrophobicity
-    hydro_bin_dic = bin_by_hydrophobe(ddg_dic)
-
-    pickle.dump(ddg_dic, open(OUT_DIR+FSEP+'hydro_bin_dic.pkl', 'wb'))
-
     # get fitness dictionary
     fitness_dic = pickle.load(open(IN_DIR+FSEP+FITNESS_PKL, "rb"))
 
-    # generate lists for corrleation
-    ddg_lst, fit_lst = dic_to_lsts(ddg_dic, fitness_dic)
+    for (name, fp) in BINDING_LST:
 
-    lsts_to_csv(ddg_lst, fit_lst, 'DDG', 'Fitness', 'all')
+        # get ddg dic
+        ddg_dic = json_to_dic(IN_DIR+FSEP+fp, param)
 
-    bin_dic_to_csv(hydro_bin_dic, fitness_dic)
+        pickle.dump(ddg_dic, open(OUT_DIR+FSEP+'ddg_dic_'+name+'.pkl', 'wb'))
 
+        # bin by hydrophobicity
+        hydro_bin_dic = bin_by_hydrophobe(ddg_dic)
 
+        pickle.dump(ddg_dic, open(OUT_DIR+FSEP+'hydro_bin_dic_'+name+'.pkl', 'wb'))
+
+        # bin by topology
+        topo_bin_dic = bin_by_topo(ddg_dic)
+
+        pickle.dump(ddg_dic, open(OUT_DIR+FSEP+'topo_bin_dic_'+name+'.pkl', 'wb'))
+
+        # generate lists for corrleation
+        ddg_lst, fit_lst = dic_to_lsts(ddg_dic, fitness_dic)
+
+        lsts_to_csv(ddg_lst, fit_lst, 'DDG', 'Fitness', 'all', name)
+
+        bin_dic_to_csv(hydro_bin_dic, fitness_dic, name)
+        bin_dic_to_csv(topo_bin_dic, fitness_dic, name)
 
 
 run()
