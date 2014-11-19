c52119b3b9b044f30ab0cc419620ad279113e657
brukmensa <brukmensa@gmail.com>
2014-11-18 16:42:32 -0800

    changed newfitness.py

--- fitness/Bruk/newfitnesses.py
+++ fitness/Bruk/newfitnesses.py
@@ -6,7 +6,7 @@ aa_dic = pic.load(open("translate.pkl", "rb"))
 #################### change name of file to input ##########################
 testset = pic.load(open("Codon_FitScore_HU_day1.pkl", "rb"))
 
-fitnessarray = np.zeros((22,78))
+fitnessarray = np.zeros((21,77))
 
 
 print fitnessarray
@@ -16,18 +16,15 @@ for key in testset.iterkeys():
 	rnacodon = codon.replace('T','U')
 	if rnacodon != 'WU':
 		aa_iden = aa_dic[rnacodon]
-	else:
-		aminonum = 21
-	if key[1] !='WT':
 		aminonum = num_dic[aa_iden]
-		fitnessarray[int(aminonum),int(key[0])] = testset[key][0]
+		fitnessarray[int(aminonum-1),int(key[0]-1)] = testset[key][0]
 print fitnessarray[1]
 
-####
+################### Change the name of the output file ######################
 filename = "HU_D1_new_fitness_scores.txt"
 
 arrayfile= open(filename, "w")
 
-
+################### Change the name of the output pickle ###################
 np.savetxt(arrayfile, fitnessarray, delimiter = "	")
 pic.dump(fitnessarray, open("HU_D1_new_fitness_scores.py", "wb"))
