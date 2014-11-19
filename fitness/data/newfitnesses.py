8af01aa09e9af3022c4ff4ac2c4e24a9d55acf11
Lillian Kenner <lkenner@fraserlab.com>
2014-11-19 11:00:42 -0800

    subtracted fitness scores

--- fitness/data/newfitnesses.py
+++ fitness/data/newfitnesses.py
@@ -4,27 +4,23 @@ num_dic = pic.load(open("aminotonumber.pkl", "rb"))
 aa_dic = pic.load(open("translate.pkl", "rb"))
 
 #################### change name of file to input ##########################
-testset = pic.load(open("Codon_FitScore_HU_day1.pkl", "rb"))
+testset = pic.load(open("Codon_FitScore_MG132_day1.pkl", "rb"))
 
 fitnessarray = np.zeros((21,77))
 
 
-print fitnessarray
-
 for key in testset.iterkeys():
-	codon = key[1]
-	rnacodon = codon.replace('T','U')
-	if rnacodon != 'WU':
+    rnacodon = key[1].replace('T','U')
+    if rnacodon != 'WU':
 		aa_iden = aa_dic[rnacodon]
 		aminonum = num_dic[aa_iden]
 		fitnessarray[int(aminonum-1),int(key[0]-1)] = testset[key][0]
-print fitnessarray[1]
 
 ################### Change the name of the output file ######################
-filename = "HU_D1_new_fitness_scores.txt"
+filename = "MG132_D1_new_fitness_scores.txt"
 
 arrayfile= open(filename, "w")
 
 ################### Change the name of the output pickle ###################
 np.savetxt(arrayfile, fitnessarray, delimiter = ",")
-pic.dump(fitnessarray, open("HU_D1_new_fitness_scores.py", "wb"))
+pic.dump(fitnessarray, open("MG132_D1_new_fitness_scores.pkl", "wb"))
--- fitness/data/subtracted_fitness.py
+++ fitness/data/subtracted_fitness.py
@@ -1,21 +1,27 @@
 import numpy as np
 import sys
 
-f1 = sys.argv[1]
-f2 = sys.argv[2]
 
 f1_mat = np.genfromtxt(sys.argv[1], delimiter=',')
+#HU data
 f2_mat = np.genfromtxt(sys.argv[2], delimiter=',')
+#other group data
 
 # HOW IT WURKS
-#N,q = f1_mat.shape
-#f1minf2 = np.zeros((N,q))
-#
-#for pos in range(N):
-#    for am in range(q):
-#        f1minf2[pos,am] = f1_mat[pos,am] - f2_mat[pos,am]
-        
-   
-f_diff = f1_mat - f2_mat
-np.savetxt("subtracted.csv", f_diff, delimiter=",")
+N,q = f1_mat.shape
+f1minf2 = np.zeros((N,q))
 
+for pos in range(N):
+    for am in range(q):
+        f1minf2[pos,am] = f1_mat[pos,am] - f2_mat[pos,am]
+        if f1_mat[pos,am] == 0. or f2_mat[pos,am] == 0.:
+            f1minf2[pos,am] == 0.
+   
+#f_diff = f1_mat - f2_mat
+np.savetxt("{0}.csv".format(sys.argv[3]), f1minf2, delimiter=",")
+#ouput csv file
+
+import os
+import matplotlib.pyplot as plt
+plt.pcolormesh(f1minf2, cmap='Spectral')
+plt.savefig("{0}.png".format(sys.argv[3]))
